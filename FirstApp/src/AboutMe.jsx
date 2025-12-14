9b9b4a08fe48075f78f2f13c607faf844964aa60
MAZOUZ ABDERRAHMANE <mazathomazigh@gmail.com>
2025-12-14 17:42:38 +0100

    I added some modifications

--- FirstApp/src/AboutMe.jsx
+++ FirstApp/src/AboutMe.jsx
@@ -7,7 +7,7 @@ function AboutMe() {
      <div className="About">
        <p>Hello, i am</p>
        <h1 className='Name'>MAZOUZ ABDERRAHMANE</h1>
-       <p>Have a project in mind or want to collaborate with me You can contact me directly by sending a message or reach out through my social media profiles I am always open to discussing new ideas projects or opportunities and I will get back to you as soon as possible</p>
+       <p>I am a web developer dedicated to building efficient, scalable, and well-designed digital solutions. My approach combines attention to detail, strong problem-solving skills, and a commitment to delivering high-quality results. Driven by curiosity and passion, I continuously enhance my knowledge and refine my development practices.</p>
        
      </div>
        
--- FirstApp/src/ContactTxt.jsx
+++ FirstApp/src/ContactTxt.jsx
@@ -6,7 +6,8 @@ function ContactTxt() {
         <>
         <div className='contact-txt'>
             <h2 className='contact-title'>Contact Me</h2>
-            <p>Have a project in mind or looking for a developer to collaborate with you I am open to new opportunities including frontend backend and desktop software development and I enjoy working on modern efficient and clean solutions Feel free to reach out and I will be happy to discuss your ideas and get back to you as soon as possible</p>
+            <p>Have a project in mind or want to collaborate with me You can contact me directly by sending a message or reach out through my social media profiles I am always open to discussing new ideas projects or opportunities and I will get back to you as soon as possible</p>
+            
         </div>
         </>
     )

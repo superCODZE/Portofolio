c7d73a337534a628936bdd27fd559495a6f69284
MAZOUZ ABDERRAHMANE <mazathomazigh@gmail.com>
2025-12-14 17:21:46 +0100

    i add some changes

--- FirstApp/src/AboutMe.jsx
+++ FirstApp/src/AboutMe.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 
+
 function AboutMe() {
    return(
     <>
@@ -7,6 +8,7 @@ function AboutMe() {
        <p>Hello, i am</p>
        <h1 className='Name'>MAZOUZ ABDERRAHMANE</h1>
        <p>Have a project in mind or want to collaborate with me You can contact me directly by sending a message or reach out through my social media profiles I am always open to discussing new ideas projects or opportunities and I will get back to you as soon as possible</p>
+       
      </div>
        
     </>
